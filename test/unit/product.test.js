7c53d72770d34d8323bf373b7f1739a3b82c0f95
n560 <n560@devin.com>
2022-11-20 20:28:01 +0900

    섹션 2 - 2강 : Create Method 로 데이터 저장하기

--- test/unit/product.test.js
+++ test/unit/product.test.js
@@ -1,9 +1,17 @@
 const productController = require('../../controller/product')
+const productModel = require('../../models/Product')
+
+productModel.create = jest.fn();
 
 describe("Product Controller Create", () => {
 
     it("should have a createProduct function", ()=> {
         expect(typeof productController.createProduct).toBe('function')
+    })
+
+    it("should call productController.createProduct", () => {
+        productController.createProduct();
+        expect(productModel.create).toBeCalled();
     })
     
 })
