eec81b05e8a07ca671f3a3eef521050960ebbd49
n560 <n560@devin.com>
2022-11-20 19:16:02 +0900

    섹션 1 : 7강 MongoDB 클러스터 생성

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const PORT = 5000
 
 const app = express()
 const productRoutes = require('./routes')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://junho:1234@testcluster1.36dil.mongodb.net/?retryWrites=true&w=majority',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => console.log('MongoDB Connected...'))
+        .catch(err => console.log(err))
+
 
 app.use(express.json())
 app.use('/api/products', productRoutes)
