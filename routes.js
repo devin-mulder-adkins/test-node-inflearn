3d0d8a645c001f270d93fd7cf3aa3098a71acdc7
foryou <foryou@devin.com>
2022-11-21 11:53:31 +0900

    섹션 5. Read - 8강. getProductById 통합 테스트 작성 (1)
    이번에 작성한 통합 테스트는 단독으로 수행되면 안된다.
    
    firstProduct 값을 그 앞의 통합테스트에서 생성되도록 의존하기 때문이다.
    
    반드시 그 앞의 통합테스트와 함게 수행되어야 한다.

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const productController = require('./controller/product')
 router.get('/', productController.getProducts)
-
+router.get('/:productId', productController.getProductById)
 router.post('/', productController.createProduct)
 
 module.exports = router
